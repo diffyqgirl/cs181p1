dffa89ab67b090521034504b544fad6cbfb80e5e
diffyqgirl <ungar@college.harvard.edu>
2018-03-03 21:38:33 -0500

    added ignore

--- P1-regression/make_test_small.py
+++ P1-regression/make_test_small.py
@@ -8,11 +8,11 @@ for i in range(1,10000):
 
 readfile.close()
 writefile.close()
-readfile = open("test.csv", 'rb')
-writefile = open("test_small.csv", 'wb')
+readfile = open("train.csv", 'rb')
+writefile = open("train2_small.csv", 'wb')
 reader = csv.reader(readfile, delimiter=' ', quotechar='|')
 writer = csv.writer(writefile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-for i in range(1,10000):
+for i in range(10000,20000):
 	writer.writerow(reader.next())
 
 readfile.close()
